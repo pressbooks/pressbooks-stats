77100946e8f07812dd3e12fbf5e8f49948799f5e
Dac Chartrand <dac.chartrand@gmail.com>
2017-05-17 14:19:27 -0400

    Respect db prefix, migrate when possible (fix #2)

--- includes/pb-helpers.php
+++ includes/pb-helpers.php
@@ -21,7 +21,7 @@ function load_template( $path, array $vars = array() ) {
 	extract( $vars ); // @codingStandardsIgnoreLine
 	include( $path );
 	$output = ob_get_contents();
-	ob_end_clean();
+	@ob_end_clean();
 
 	return $output;
 }
@@ -30,15 +30,40 @@ function load_template( $path, array $vars = array() ) {
 // Activation & Deactivation
 // --------------------------------------------------------------------------------------------------------------------
 
+/**
+ * Return the current version of the stats database
+ *
+ * @return string
+ */
+function get_db_version() {
+	return '1.5';
+}
+
+/**
+ * Return the current name of the stats table
+ *
+ * @return string
+ */
+function get_stats_table() {
+	/** @var $wpdb \wpdb */
+	global $wpdb;
+	return "{$wpdb->base_prefix}pressbooks_stats_exports";
+}
+
+/**
+ * Create a new table for stats
+ */
 function install() {
 
-	$db_version = '1.4';
-
 	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-	$table_name = 'wp_pressbooks_stats_exports';
 	$installed_ver = get_option( 'pb_stats_db_version' );
+	$db_version = get_db_version();
 
 	if ( $installed_ver != $db_version ) {
+
+		rename_table();
+
+		$table_name = get_stats_table();
 		$sql = "CREATE TABLE {$table_name} (
                id mediumint(9) NOT NULL AUTO_INCREMENT,
                blog_id mediumint(9) NOT NULL,
@@ -55,4 +80,28 @@ function install() {
 		update_option( 'pb_stats_db_version', $db_version );
 	}
 
+}
+
+/**
+ * Rename old hard coded table
+ */
+function rename_table() {
+
+	/** @var \wpdb $wpdb */
+	global $wpdb;
+
+	$oldTableName = 'wp_pressbooks_stats_exports';
+
+	if ( $oldTableName == get_stats_table() ) {
+		// The old and new table names are the same, ignore
+		return;
+	}
+
+	$checkIfOldTableExistsSql = "SELECT 1 FROM {$oldTableName} LIMIT 1 ";
+	$wpdb->get_results( $checkIfOldTableExistsSql, ARRAY_A );
+	if ( ! $wpdb->last_error ) {
+		// The old hard coded table exists, rename it
+		$renameTableSql = "RENAME TABLE {$oldTableName} TO " . get_stats_table();
+		$wpdb->query( $renameTableSql );
+	}
 }
