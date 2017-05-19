cdf4c0f9490ebbda15ae134cf195e5df93d2fd90
Ned Zimmerman <ned@bight.ca>
2017-05-19 09:33:15 -0300

    Bump version.

--- pressbooks-stats.php
+++ pressbooks-stats.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Pressbooks Stats
 Description: Pressbooks plugin which provides some basic activity statistics for a Pressbooks network.
-Version: 1.2.1
+Version: 1.3.0
 Author: BookOven Inc.
 */
 
