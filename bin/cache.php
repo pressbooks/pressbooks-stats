7b94ab4d19b12770163f0ef3227a78083a070028
Ned Zimmerman <ned@bight.ca>
2018-09-18 21:48:36 -0300

    Cache network storage, use du to retrieve (fixes #11) (#12)
    * Cache network storage, use du to retrieve (fixes #11)
    
    * Fix CS error

--- bin/cache.php
+++ bin/cache.php
@@ -7,6 +7,8 @@ if ( ! defined( 'WP_CLI' ) ) {
 
 set_time_limit( 0 );
 
+// Stats page
+
 $cache_key = \PressbooksStats\Stats\get_admin_page_html_cache_key();
 
 delete_site_transient( $cache_key );
@@ -19,4 +21,20 @@ if ( empty( $html ) ) {
 	echo "Failed to cache the stats dashboard... \n";
 } else {
 	echo "Successfully cached the stats dashboard! \n";
+}
+
+// Network storage
+
+$cache_key = 'pb_stats_network_storage';
+
+delete_site_transient( $cache_key );
+
+\PressbooksStats\Stats\cache_network_storage();
+
+$storage = get_site_transient( $cache_key );
+
+if ( empty( $storage ) ) {
+	echo "Failed to cache network storage data... \n";
+} else {
+	echo "Successfully cached network storage data! \n";
 }
