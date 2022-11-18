6b31340df7be3ad1992c32b3f7d3cd3256907c14
Steel Wagstaff <steel@pressbooks.com>
2022-11-17 16:47:42 -0800

    Require PHP 8 and bump dependencies (#30)
    * update php compatibility version
    
    * Require PHP 8 and bump dependencies

--- pressbooks-stats.php
+++ pressbooks-stats.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Pressbooks Stats
 Description: Pressbooks plugin which provides some basic activity statistics for a Pressbooks network.
-Version: 1.7.0
-Pressbooks tested up to: 6.3.0
+Version: 1.8.0
+Pressbooks tested up to: 6.4.0
 Author: Pressbooks (Book Oven Inc.)
 Author URI: https://pressbooks.org
 Network: True
